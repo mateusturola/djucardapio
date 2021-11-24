d7c08987b2633a879a67fb11cfd01c1110bf5934
mateusturola <turolamateus@gmail.com>
2021-11-24 16:39:02 -0300

    Add Dados do restaurante

---
+++ src/services/contact-data.jsx
@@ -0,0 +1,22 @@
+export const contactDetails = {
+  name: "Pizzaria D'Ju",
+  slogan: 'Uma nova opção de pizzaria, com qualidade e dedicação direto pra sua residência.',
+  address: 'Rua Joaquim Fagundes Ferreira, 1060 Vila Ursulino, Barra Mansa/ RJ',
+  whatsApp: {
+    icon: 'fab fa-whatsapp',
+    name: '24 99968-5136',
+    link: 'https://api.whatsapp.com/send?phone=5524999685136',
+  },
+  social: [
+    {
+      icon: 'fab fa-instagram-square social',
+      name: 'Instagram',
+      link: 'https://www.instagram.com/pizzasdju/',
+    },
+    {
+      icon: 'fab fa-facebook-square social',
+      name: 'Facebook',
+      link: 'https://web.facebook.com/DJ%C3%BA-967224659995044',
+    },
+  ],
+};
